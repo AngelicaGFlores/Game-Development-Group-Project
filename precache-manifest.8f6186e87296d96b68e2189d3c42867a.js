32f549e67e91ac5a945cc92cf69c423351d85d42
AngelicaGFlores <33407052+AngelicaGFlores@users.noreply.github.com>
2020-09-27 18:19:58 -0700

    Updates

---
+++ precache-manifest.8f6186e87296d96b68e2189d3c42867a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a6bef69c30bda734d2b3c39d2b5761",
+    "url": "/Game-Development-Group-Project/index.html"
+  },
+  {
+    "revision": "8d81848ba6433ce71969",
+    "url": "/Game-Development-Group-Project/static/css/2.8e1df220.chunk.css"
+  },
+  {
+    "revision": "35b3892f00cb7dfc4708",
+    "url": "/Game-Development-Group-Project/static/css/main.07774ee3.chunk.css"
+  },
+  {
+    "revision": "8d81848ba6433ce71969",
+    "url": "/Game-Development-Group-Project/static/js/2.c4285873.chunk.js"
+  },
+  {
+    "revision": "35b3892f00cb7dfc4708",
+    "url": "/Game-Development-Group-Project/static/js/main.20136ccb.chunk.js"
+  },
+  {
+    "revision": "ed8b6b3f552f9dbdbdf9",
+    "url": "/Game-Development-Group-Project/static/js/runtime-main.c218de9c.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Game-Development-Group-Project/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Game-Development-Group-Project/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Game-Development-Group-Project/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Game-Development-Group-Project/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Game-Development-Group-Project/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
